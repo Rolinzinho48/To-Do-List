efe44c92ce4ef9dab2e379e762ba7a89a08e1e8b
Izaque Rolim <98198585+Rolinzinho48@users.noreply.github.com>
2022-04-04 18:17:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let listaTarefas = []
 
     DataBase.transaction(function(inicial){
          inicial.executeSql("CREATE TABLE tarefinha(id INTEGER PRIMARY KEY, conteudo VARCHAR(255))")
-         console.log("A")
     })
 
 
@@ -23,7 +22,11 @@ function AdicionarSQL(conteudo){
     DataBase.transaction(function(inserir){
         inserir.executeSql("INSERT INTO tarefinha(conteudo) VALUES(?)",[conteudo])
     })
-    
+}
+function DeletaSQL(nome){
+    DataBase.transaction(function(deletar){
+        deletar.executeSql("DELETE FROM tarefinha WHERE conteudo = ?",[nome])
+    })
 }
 
 function listarTarefas(txtTarefa){
@@ -48,21 +51,23 @@ function listarTarefas(txtTarefa){
     document.querySelector("ul").appendChild(Tarefa)
 }
 
-function CriarTarefa(){
+function CriarTarefa(txtTarefa){
     
-    let txtTarefa = document.getElementById("inputText").value
     let Tarefa = document.createElement("li")
+    let div = document.createElement("div")
     let Tarefa2 = document.createElement("label")
   
     let check = document.createElement("input")
     let botao = document.createElement("button")
 
+    check.addEventListener("click",sublinharTexto)
     botao.addEventListener("click",RemoverTarefa)
     
     check.type = "checkbox"
     Tarefa2.innerHTML = txtTarefa
-    botao.innerText = "Remover "
+    botao.innerText = "x"
     
+  
 
     Tarefa.appendChild(Tarefa2)
     Tarefa.appendChild(check)
@@ -72,12 +77,41 @@ function CriarTarefa(){
     document.querySelector("ul").appendChild(Tarefa)
     listaTarefas.push(Tarefa)
     AdicionarSQL(txtTarefa)
+
 }
 
+ sublinharTexto =(event) =>{
+    if(event.target.checked == true){
+        event.target.parentElement.children[0].style.textDecoration = "line-through"
+        event.target.parentElement.children[0].style.fontStyle = "italic"
+    }
+    else{
+        event.target.parentElement.children[0].style.textDecoration = "none"
+        event.target.parentElement.children[0].style.fontStyle = ""
+    }
+}
 function RemoverTarefa(event){
-    event.target.parentElement.remove()  
+    event.target.parentElement.remove()     
+
+    let a = event.target.parentElement.children[0].textContent
+
+    DeletaSQL(a)
+
 }
 
+function GeradorTarefa(){
+    let txt = document.getElementById("inputText").value
+    CriarTarefa(txt)
+    document.getElementById("inputText").value = ""
+}
+
+function sla(event){
+    let tecla = event.keyCode
+    if(tecla == 13){
+        GeradorTarefa() 
+    }
+}
 
 let btn = document.getElementById("botao1")
-btn.addEventListener("click",CriarTarefa);
+btn.addEventListener("click",GeradorTarefa);
+let chama = document.querySelector("inputText")
